58bdb06421b84a66d5fdcf5a0ba16039ecbb53e8
Petar Zecevic <pnz.vodka@gmail.com>
2019-03-19 18:15:08 +0100

    added content and values to each display

--- WeatherForecast/app/src/main/java/com/example/weatherforecast/DetailsActivity.java
+++ WeatherForecast/app/src/main/java/com/example/weatherforecast/DetailsActivity.java
@@ -1,20 +1,27 @@
 package com.example.weatherforecast;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
-
 import java.util.Calendar;
 
-public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener{
     private TextView mLocation, mDay;
-    private Button mTemperatureButton, mSunriseButton, mWindButton;
-    private LinearLayout mTemperatureDisplay, mWindDisplay, mSunriseDisplay;
+    private Button mTemperatureOption, mSunriseOption, mWindOption;
+    private Display mWindDisplay, mSunriseDisplay;
+
+    private class TemperatureDisplay extends Display{
+        public ArrayAdapter<String> unitsAdapter;
+        public String scaleSelected;
+    }
+    private TemperatureDisplay mTemperatureDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +35,64 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
 
         mLocation.setText((CharSequence)locationName);
         mDay.setText(parseDay((Calendar.DAY_OF_WEEK+1)%7));
-        // Set displays.
-        mTemperatureDisplay = findViewById(R.id.temp_display);
-        mWindDisplay = findViewById(R.id.wind_display);
-        mSunriseDisplay = findViewById(R.id.sunrise_display);
 
-        mTemperatureDisplay.setVisibility(View.INVISIBLE);
-        mWindDisplay.setVisibility(View.INVISIBLE);
-        mSunriseDisplay.setVisibility(View.INVISIBLE);
+        initDisplays();
+        initOptions();
+    }
 
-        // Set buttons.
-        mTemperatureButton = findViewById(R.id.temp_button);
-        mSunriseButton = findViewById(R.id.sunrise_button);
-        mWindButton = findViewById(R.id.wind_button);
+    private void initDisplays(){
+        // Temperature display.
+        mTemperatureDisplay = new TemperatureDisplay();
+        mTemperatureDisplay.itemsHolder = findViewById(R.id.temp_display);
+        mTemperatureDisplay.unitsAdapter = new ArrayAdapter<String>(this,
+                                                R.layout.support_simple_spinner_dropdown_item);
+        Spinner list = findViewById(R.id.list_units);
+        list.setAdapter(mTemperatureDisplay.unitsAdapter);
+        list.setOnItemSelectedListener(this);
+        mTemperatureDisplay.unitsAdapter.add("C");
+        mTemperatureDisplay.unitsAdapter.add("F");
+        mTemperatureDisplay.items.put("temperature", (TextView) findViewById(R.id.temp_val));
+        mTemperatureDisplay.values.put("temperature", "25");
+        mTemperatureDisplay.items.put("humidity", (TextView) findViewById(R.id.humidity_val));
+        mTemperatureDisplay.values.put("humidity", "1000");
+        mTemperatureDisplay.items.put("pressure",(TextView) findViewById(R.id.pressure_val));
+        mTemperatureDisplay.values.put("pressure", "1013");
+        mTemperatureDisplay.setValues();
+        // Celisus by default
+        mTemperatureDisplay.scaleSelected = "C";
+        mTemperatureDisplay.show(Boolean.FALSE);
 
-        mTemperatureButton.setOnClickListener(this);
-        mSunriseButton.setOnClickListener(this);
-        mWindButton.setOnClickListener(this);
+        // Sunrise display.
+        mSunriseDisplay = new Display();
+        mSunriseDisplay.itemsHolder = findViewById(R.id.sunrise_display);
+        mSunriseDisplay.items.put("sunrise_up", (TextView) findViewById(R.id.sunrise_up));
+        mSunriseDisplay.values.put("sunrise_up", "6:00");
+        mSunriseDisplay.items.put("sunrise_down", (TextView) findViewById(R.id.sunrise_down));
+        mSunriseDisplay.values.put("sunrise_down", "18:00");
+        mSunriseDisplay.setValues();
+        mSunriseDisplay.show(Boolean.FALSE);
+
+        // Wind display.
+        mWindDisplay = new Display();
+        mWindDisplay.itemsHolder = findViewById(R.id.wind_display);
+        mWindDisplay.items.put("wind_speed", (TextView) findViewById(R.id.wind_speed));
+        mWindDisplay.values.put("wind_speed", "10");
+        mWindDisplay.items.put("wind_direction", (TextView) findViewById(R.id.wind_diretion));
+        mWindDisplay.values.put("wind_direction", "N");
+        mWindDisplay.setValues();
+        mWindDisplay.show(Boolean.FALSE);
+
+    }
+
+    private void initOptions(){
+        mTemperatureOption = findViewById(R.id.temp_button);
+        mTemperatureOption.setOnClickListener(this);
+
+        mSunriseOption = findViewById(R.id.sunrise_button);
+        mSunriseOption.setOnClickListener(this);
+
+        mWindOption = findViewById(R.id.wind_button);
+        mWindOption.setOnClickListener(this);
     }
 
     private CharSequence parseDay(int d){
@@ -82,37 +130,57 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.temp_button:
-                displayTemperatureScreen();
+                mTemperatureOption.setBackgroundColor(Color.BLUE);
+                mSunriseOption.setBackgroundColor(Color.GRAY);
+                mWindOption.setBackgroundColor(Color.GRAY);
+
+                mTemperatureDisplay.show(Boolean.TRUE);
+                mWindDisplay.show(Boolean.FALSE);
+                mSunriseDisplay.show(Boolean.FALSE);
                 break;
             case R.id.sunrise_button:
-                displaySunriseScreen();
+                mTemperatureOption.setBackgroundColor(Color.GRAY);
+                mSunriseOption.setBackgroundColor(Color.BLUE);
+                mWindOption.setBackgroundColor(Color.GRAY);
+
+                mTemperatureDisplay.show(Boolean.FALSE);
+                mSunriseDisplay.show(Boolean.TRUE);
+                mWindDisplay.show(Boolean.FALSE);
                 break;
             case R.id.wind_button:
-                displayWindScreen();
+                mTemperatureOption.setBackgroundColor(Color.GRAY);
+                mSunriseOption.setBackgroundColor(Color.GRAY);
+                mWindOption.setBackgroundColor(Color.BLUE);
+
+                mTemperatureDisplay.show(Boolean.FALSE);
+                mSunriseDisplay.show(Boolean.FALSE);
+                mWindDisplay.show(Boolean.TRUE);
                 break;
             default:
                 break;
         }
     }
 
-    private void displayTemperatureScreen()
-    {
-        mTemperatureDisplay.setVisibility(View.VISIBLE);
-        mWindDisplay.setVisibility(View.INVISIBLE);
-        mSunriseDisplay.setVisibility(View.INVISIBLE);
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String item = mTemperatureDisplay.unitsAdapter.getItem(i);
+        if(!mTemperatureDisplay.scaleSelected.equals(item)){
+            double val = Double.valueOf(mTemperatureDisplay.values.get("temperature"));
+            if(item.equals("F")){
+                val = val * 1.8 + 32.0;
+            }
+            else{
+                val = (val - 32.0) / 1.8;
+            }
+            // Update value in view object.
+            String s = mTemperatureDisplay.values.get("temperature");
+            s = String.valueOf(val);
+            mTemperatureDisplay.setValues();
+        }
     }
 
-    private void displayWindScreen()
-    {
-        mTemperatureDisplay.setVisibility(View.INVISIBLE);
-        mWindDisplay.setVisibility(View.VISIBLE);
-        mSunriseDisplay.setVisibility(View.INVISIBLE);
-    }
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
-    private void displaySunriseScreen()
-    {
-        mTemperatureDisplay.setVisibility(View.INVISIBLE);
-        mWindDisplay.setVisibility(View.INVISIBLE);
-        mSunriseDisplay.setVisibility(View.VISIBLE);
     }
 }
---
+++ WeatherForecast/app/src/main/java/com/example/weatherforecast/Display.java
@@ -0,0 +1,34 @@
+package com.example.weatherforecast;
+
+import java.util.HashMap;
+
+import android.util.Pair;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+
+public class Display {
+    public HashMap<String, TextView> items;
+    public HashMap<String, String> values;
+    public LinearLayout itemsHolder;
+
+    public void show(Boolean yes) {
+        itemsHolder.setVisibility(yes ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    public Display(){
+        items = new HashMap<String, TextView>();
+        values = new HashMap<String, String>();
+    }
+
+    /**
+     * Method assumes that both maps have the same keys.
+     */
+    public void setValues(){
+        for(String k : values.keySet()) {
+            if (items.containsKey(k)) {
+                items.get(k).setText((CharSequence) values.get(k));
+            }
+        }
+    }
+}
